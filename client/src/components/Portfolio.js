28cc1c6596db4e6df79b4b57319b329949fc1ab1
Rafael Furry <Rafa@Rafaels-MacBook-Pro-2.local>
2018-07-13 10:41:51 -0700

    Update Portfolio text

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -15,12 +15,15 @@ const Portfolio = () => (
               </Header>
               <Image src="../../../off-the-record-referral.png" size="large" />
               <p style={{ marginTop: '10px' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Off The Record leverages the power of mobile and web technology
+                to make fighting your ticket as easy as ordering take-out. Users
+                provide a photo of their ticket and answer a few simple
+                questions, and the platform intelligently matches them with best
+                local traffic lawyer for their case. From there, everything is
+                taken care of. Users receive real-time updates on the status of
+                their case, and can communicate with their lawyer at any time
+                via the app. Once a case is resolved, the user is notified of
+                the outcome and advised of any further action.
               </p>
             </List.Item>
           </Grid.Column>
@@ -32,12 +35,10 @@ const Portfolio = () => (
               </Header>
               <Image src="../../../senate-stacks.png" size="large" />
               <p style={{ marginTop: '10px' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Senate stacks is a web app which allows users to easily find
+                campaign finance data for legislators in the United States. I
+                use OpenSecrets trusted information and display it in an
+                intuitive easy to navigate user interface.
               </p>
             </List.Item>
           </Grid.Column>
