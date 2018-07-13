577520cd7e1322fdd6e568e45c00627b9ad84db6
Rafael Furry <Rafa@Rafaels-MacBook-Pro-2.local>
2018-07-12 21:13:50 -0700

    Set up nodemailer in the server

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const { createServer } = require('http');
 const express = require('express');
 const compression = require('compression');
+const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config();
 
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
+
+var routes = require('./routes');
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', routes);
 
 app.use(express.static(path.resolve(__dirname, 'client/build')));
 
@@ -21,6 +29,5 @@ const server = createServer(app);
 
 server.listen(PORT, err => {
   if (err) throw err;
-
   console.log('Server started!');
 });
