952e57f1c6fa2d7c0a85034ee88c4c0bdfca5783
Rafael Furry <Rafa@Rafaels-MacBook-Pro-2.local>
2018-07-13 09:18:13 -0700

    Update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container, Grid } from 'semantic-ui-react';
+import SiteHeader from './components/SiteHeader';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import './App.css';
 
 const App = () => (
-  <div>
-    <h1>Portforlio</h1>
-  </div>
+  <Container className="site-container" style={{ width: '80%' }}>
+    <BrowserRouter>
+      <Grid>
+        <Grid.Column width={4}>
+          <SiteHeader />
+        </Grid.Column>
+        <Grid.Column width={11}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Grid.Column>
+      </Grid>
+    </BrowserRouter>
+  </Container>
 );
 
 export default App;
